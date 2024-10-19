f375fdae7cdf08062a199e9ec30cc065f8355823
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-10-19 10:53:01 +0530

    add: components and basic pages

---
+++ src/components/navbar.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+
+export function Navbar() {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="flex h-14 items-center p-4">
+        <div className="mr-4 hidden md:flex">
+          <Link className="mr-6 flex items-center space-x-2" href="/">
+            <span className="hidden font-bold sm:inline-block">Quizr</span>
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+          <div className="w-full flex-1 md:w-auto md:flex-none">
+            <Link className="mr-6 flex items-center space-x-2 md:hidden" href="/">
+              <span className="font-bold">Quizr</span>
+            </Link>
+          </div>
+          <nav className="flex items-center">
+            <Button
+              variant="ghost"
+              size="icon"
+              aria-label="Toggle Theme"
+              className="mr-6"
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+              <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle Theme</span>
+            </Button>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
